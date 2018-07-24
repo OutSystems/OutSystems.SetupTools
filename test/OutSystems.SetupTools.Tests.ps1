71a4027b622f85882495f8ff3886f63a2040cc75
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-07-24 11:38:40 +0100

    added module test

---
+++ test/OutSystems.SetupTools.Tests.ps1
@@ -0,0 +1,10 @@
+$ModuleManifestName = 'OutSystems.SetupTools.psd1'
+$ModuleManifestPath = "$PSScriptRoot\..\src\OutSystems.SetupTools\$ModuleManifestName"
+
+Describe 'Module Manifest Tests' {
+    It 'Passes Test-ModuleManifest' {
+        Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
+        $? | Should Be $true
+    }
+}
+
