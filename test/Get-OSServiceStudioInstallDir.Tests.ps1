66e8db5309cf9cce02ba2d36da27dc926438bd37
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-07-25 22:14:09 +0100

    Improvements on code

--- test/Get-OSServiceStudioInstallDir.Tests.ps1
+++ test/Get-OSServiceStudioInstallDir.Tests.ps1
@@ -4,16 +4,35 @@ Import-Module .\..\src\Outsystems.SetupTools
 InModuleScope -ModuleName OutSystems.SetupTools {
     Describe 'Get-OSServiceStudioInstallDir Tests' {
 
-        Context 'Service Studio not installed' {
-            It 'Service Studio 1.0 not installed' {
+        Context 'Real GetServiceStudioInstallDir' {
+
+            It 'Checks that Service Studio 1.0 is not installed' {
                { Get-OSServiceStudioInstallDir -MajorVersion '1.0' } | Should Throw "Outsystems development environment 1.0 is not installed"
             }
+
         }
 
-        Context 'Service Studio installed' {
-            Mock GetServiceStudioInstallDir { return 'C:\Program Files\OutSystems\Development Environment 10.0\Service Studio' }
-            It 'Service Studio 10 installed' {
+        Context 'Mocked GetServiceStudioInstallDir' {
+
+            Mock GetServiceStudioInstallDir {
+                return 'C:\Program Files\OutSystems\Development Environment 10.0\Service Studio'
+            }
+
+            It 'Checks that Service Studio 10 installed' {
                 Get-OSServiceStudioInstallDir -MajorVersion '10.0' | Should Be 'C:\Program Files\OutSystems\Development Environment 10.0\Service Studio'
+            }
+
+            It 'Checks that GetServiceStudioInstallDir is called only once' {
+
+                $assMParams = @{
+                    'CommandName' = 'GetServiceStudioInstallDir'
+                    'Times' = 1
+                    'Exactly' = $true
+                    'Scope' = 'Context'
+                    'ParameterFilter' = { $MajorVersion -eq '10.0' }
+                 }
+
+                 Assert-MockCalled @assMParams
             }
         }
     }
