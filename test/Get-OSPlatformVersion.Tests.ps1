d2676438634083f6c7a05e52798ea3ba1e0b0709
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-07-25 22:13:44 +0100

    Improvements on code

--- test/Get-OSPlatformVersion.Tests.ps1
+++ test/Get-OSPlatformVersion.Tests.ps1
@@ -5,19 +5,25 @@ InModuleScope -ModuleName OutSystems.SetupTools {
     Describe 'Get-OSPlatformVersion Tests' {
 
         Context 'Real WebServiceProxy' {
-            It 'Service Center not reachable' {
+
+            It 'Test the result when Service Center is NOT reachable' {
                 { Get-OSPlatformVersion -Host 255.255.255.255 } | Should Throw "Error contacting service center"
             }
+
         }
+
         Context 'Mocked WebServiceProxy' {
+
             Mock New-WebServiceProxy {
                 $obj = New-MockObject -Type 'System.Web.Services.Protocols.SoapHttpClientProtocol'
                 $obj | Add-Member -MemberType ScriptMethod -Name 'GetPlatformInfo' -Force -Value { '10.0.0.1' }
                 return $obj
             }
-            It 'Service Center reachable' {
+
+            It 'Test the result if Service Center is reachable' {
                 Get-OSPlatformVersion -Host csdevops-dev.outsystems.net | Should Be '10.0.0.1'
             }
+
         }
     }
 }
