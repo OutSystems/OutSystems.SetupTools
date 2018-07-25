0fa8357541f50cde1dc1a278be3dc30fcfbcfcaa
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-07-25 22:14:30 +0100

    Improvements on code

--- test/Get-OSServiceStudioVersion.Tests.ps1
+++ test/Get-OSServiceStudioVersion.Tests.ps1
@@ -4,16 +4,33 @@ Import-Module .\..\src\Outsystems.SetupTools
 InModuleScope -ModuleName OutSystems.SetupTools {
     Describe 'Get-OSServiceStudioVersion Tests' {
 
-        Context 'Service Studio not installed' {
-            It 'Service Studio 1.0 not installed' {
+        Context 'Real GetServiceStudioVersion' {
+
+            It 'Checks that Service Studio 1.0 not installed' {
                { Get-OSServiceStudioVersion -MajorVersion '1.0' } | Should Throw "Outsystems development environment 1.0 is not installed"
             }
+
         }
 
-        Context 'Service Studio installed' {
+        Context 'Mocked GetServiceStudioVersion' {
+
             Mock GetServiceStudioVersion { return '10.0.822.0' }
-            It 'Service Studio 10 installed' {
+
+            It 'Checks that Service Studio 10 installed' {
                 Get-OSServiceStudioVersion -MajorVersion '10.0' | Should Be '10.0.822.0'
+            }
+
+            It 'Checks that GetServiceStudioInstallDir is called only once' {
+
+                $assMParams = @{
+                    'CommandName' = 'GetServiceStudioVersion'
+                    'Times' = 1
+                    'Exactly' = $true
+                    'Scope' = 'Context'
+                    'ParameterFilter' = { $MajorVersion -eq '10.0' }
+                 }
+
+                 Assert-MockCalled @assMParams
             }
         }
     }
