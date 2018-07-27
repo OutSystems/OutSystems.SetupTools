74095b5fc577f8ca98406556b019dd670488b983
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-07-27 10:03:05 +0100

    Added Get-OSPlatformApplications tests

---
+++ test/Get-OSPlatformApplications.Tests.ps1
@@ -0,0 +1,35 @@
+Get-Module Outsystems.SetupTools | Remove-Module -Force
+Import-Module .\..\src\Outsystems.SetupTools
+
+InModuleScope -ModuleName OutSystems.SetupTools {
+    Describe 'Get-OSPlatformApplications Tests' {
+
+        Context 'When cannot connect to service center or the webservice returns error' {
+
+            Mock GetPlatformServicesWS { Throw "Error" }
+
+            It 'Should throw an error' {
+                { Get-OSPlatformApplications -ServiceCenterHost 255.255.255.255 -ServiceCenterUser "admin" -ServiceCenterPass "admin" } | Should Throw "Error getting applications"
+            }
+
+        }
+
+        Context 'Can connect and get results' {
+
+            Mock GetPlatformServicesWS {
+                $obj = New-MockObject -Type 'System.Web.Services.Protocols.SoapHttpClientProtocol'
+                $obj | Add-Member -MemberType ScriptMethod -Name 'Applications_Get' -Force -Value { @{ 'Name' = 'MyApp'; 'Key' = 'c36e9646-0caf-4510-ad35-28a8b97c28b8' } }
+                return $obj
+            }
+
+            It 'Should get the app name' {
+                $(Get-OSPlatformApplications -ServiceCenterHost 255.255.255.255 -ServiceCenterUser "admin" -ServiceCenterPass "admin").Name | Should Be "MyApp"
+            }
+
+            It 'Should get the app key' {
+                $(Get-OSPlatformApplications -ServiceCenterHost 255.255.255.255 -ServiceCenterUser "admin" -ServiceCenterPass "admin").Key | Should Be "c36e9646-0caf-4510-ad35-28a8b97c28b8"
+            }
+
+        }
+    }
+}
