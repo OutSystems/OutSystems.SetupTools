8526ebc695e6d8f403b7675324ff286b024aa256
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-08-21 16:37:32 +0100

    Code formatting

--- src/Outsystems.SetupTools/Functions/Install-OSPlatformServiceCenter.ps1
+++ src/Outsystems.SetupTools/Functions/Install-OSPlatformServiceCenter.ps1
@@ -20,60 +20,58 @@ Function Install-OSPlatformServiceCenter {
         [switch]$Force
     )
 
-    Begin {
+    begin {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Starting"
         Write-Output "Installing Outsystems Service Center. This can take a while... Please wait..."
-        Try {
+        try {
             CheckRunAsAdmin | Out-Null
-        }
-        Catch {
+        } catch {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "The current user is not Administrator or not running this script in an elevated session"
-            Throw "The current user is not Administrator or not running this script in an elevated session"
+            throw "The current user is not Administrator or not running this script in an elevated session"
         }
 
-        Try {
+        try {
             $OSVersion = GetServerVersion
             GetServerInstallDir | Out-Null
-        }
-        Catch {
+        } catch {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Outsystems platform is not installed"
-            Throw "Outsystems platform is not installed"
+            throw "Outsystems platform is not installed"
         }
 
-        Try {
+        try {
             $SCVersion = GetSCCompiledVersion
+        } catch {
         }
-        Catch {}
 
-        If ( $SCVersion -ne $OSVersion ) {
+        if ( $SCVersion -ne $OSVersion ) {
             $DoInstall = $true
-        }
-        Else {
+        } else {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Service Center was already compiled with this server version"
         }
     }
 
-    Process {
-        If ($DoInstall -or $Force.IsPresent) {
+    process {
+        if ($DoInstall -or $Force.IsPresent) {
 
-            If( $Force.IsPresent ){ LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Force switch specified. We will reinstall!!" }
+            if ( $Force.IsPresent ) {
+                LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Force switch specified. We will reinstall!!"
+            }
 
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Installing Outsystems Service Center. This can take a while..."
-            Try {
+            try {
                 $Result = RunSCInstaller -Arguments "-file ServiceCenter.oml -extension OMLProcessor.xif IntegrationStudio.xif"
-            }
-            Catch {
+            } catch {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Error lauching the service center installer"
-                Throw "Error lauching the service center installer"
+                throw "Error lauching the service center installer"
             }
 
             $OutputLog = $($Result.Output) -Split ("`r`n")
-            ForEach ($Logline in $OutputLog) {
+            foreach ($Logline in $OutputLog) {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "SCINSTALLER: $Logline"
             }
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "SCInstaller exit code: $($Result.ExitCode)"
 
-            If ( $Result.ExitCode -ne 0 ) {
+            if ( $Result.ExitCode -ne 0 ) {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Error installing service center. Return code: $($Result.ExitCode)"
                 throw "Error installing service center. Return code: $($Result.ExitCode)"
             }
@@ -83,7 +81,7 @@ Function Install-OSPlatformServiceCenter {
         }
     }
 
-    End {
+    end {
         Write-Output "Outystems Service Center successfully installed!!"
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 2 -Stream 0 -Message "Ending"
     }
