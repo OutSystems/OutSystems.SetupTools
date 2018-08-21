8579bdd045e9312d60e0307487581c518a3f17d5
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-08-21 18:11:52 +0100

    code formatting

--- src/Outsystems.SetupTools/Functions/Publish-OSPlatformLifetime.ps1
+++ src/Outsystems.SetupTools/Functions/Publish-OSPlatformLifetime.ps1
@@ -1,11 +1,11 @@
-Function Publish-OSPlatformLifetime {
+function Publish-OSPlatformLifetime {
     <#
     .SYNOPSIS
     Install or update Outsystems Lifetime.
 
     .DESCRIPTION
     This will install or update Lifetime.
-    You need to specify a user and a password to connect to Service Center. If you dont specify, the default admin will be used.
+    You need to specify a user and a password to connect to Service Center. if you dont specify, the default admin will be used.
     It will skip the installation if already installed with the right version.
     Service Center needs to be installed using the Install-OSPlatformServiceCenter function.
     Outsystems system components needs to be installed using the Publish-OSPlatformSystemComponents function.
@@ -36,73 +36,73 @@ Function Publish-OSPlatformLifetime {
         [string]$ServiceCenterPass = $OSSCPass
     )
 
-    Begin {
+    begin {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Starting"
         Write-Output "Starting Lifetime installation. This can take a while... Please wait..."
-        Try {
+        try {
             CheckRunAsAdmin | Out-Null
         }
-        Catch {
+        catch {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "The current user is not Administrator or not running this script in an elevated session"
-            Throw "The current user is not Administrator or not running this script in an elevated session"
+            throw "The current user is not Administrator or not running this script in an elevated session"
         }
 
-        Try {
+        try {
             $OSVersion = GetServerVersion
             $OSInstallDir = GetServerInstallDir
         }
-        Catch {
+        catch {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Outsystems platform is not installed"
-            Throw "Outsystems platform is not installed"
+            throw "Outsystems platform is not installed"
         }
 
-        Try {
+        try {
             $SCVersion = GetSCCompiledVersion
             $SystemComponentsVersion = GetSysComponentsCompiledVersion
             $LifetimeVersion = GetLifetimeCompiledVersion
         }
-        Catch {}
+        catch {}
 
-        If ( $SCVersion -ne $OSVersion ) {
+        if ( $SCVersion -ne $OSVersion ) {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Service Center version mismatch. You should run the Install-OSPlatformServiceCenter first"
             throw "Service Center version mismatch. You should run the Install-OSPlatformServiceCenter first"
         }
 
-        If ( $SystemComponentsVersion -ne $OSVersion ) {
+        if ( $SystemComponentsVersion -ne $OSVersion ) {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Systems components version mismatch. You should run the Publish-OSPlatformSystemComponents first"
             throw "Systems components version mismatch. You should run the Publish-OSPlatformSystemComponents first"
         }
 
-        If ( $LifetimeVersion -ne $OSVersion ) {
+        if ( $LifetimeVersion -ne $OSVersion ) {
             $DoInstall = $true
         }
-        Else {
+        else {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Lifetime was already compiled with this server version"
         }
     }
 
-    Process {
+    process {
 
-        If ($DoInstall -or $Force.IsPresent) {
+        if ($DoInstall -or $Force.IsPresent) {
 
-            If( $Force.IsPresent ){ LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Force switch specified. We will reinstall!!" }
+            if( $Force.IsPresent ){ LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Force switch specified. We will reinstall!!" }
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Installing Lifetime. This can take a while..."
 
-            Try {
+            try {
                 $Result = PublishSolution -Solution "$OSInstallDir\Lifetime.osp" -SCUser $ServiceCenterUser -SCPass $ServiceCenterPass
             }
-            Catch {
+            catch {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Error lauching the lifetime installer"
-                Throw "Error lauching the lifetime installer"
+                throw "Error lauching the lifetime installer"
             }
 
             $OutputLog = $($Result.Output) -Split ("`r`n")
-            ForEach ($Logline in $OutputLog) {
+            foreach ($Logline in $OutputLog) {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "OSPTOOL: $Logline"
             }
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "OSPTool exit code: $($Result.ExitCode)"
 
-            If ( $Result.ExitCode -ne 0 ) {
+            if ($Result.ExitCode -ne 0) {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Error installing lifetime. Return code: $($Result.ExitCode)"
                 throw "Error installing lifetime. Return code: $($Result.ExitCode)"
             }
@@ -112,7 +112,7 @@ Function Publish-OSPlatformLifetime {
         }
     }
 
-    End {
+    end {
         Write-Output "Outystems Lifetime successfully installed!!"
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 2 -Stream 0 -Message "Ending"
     }
