f3c858aa43d92565aedea1ae01086c3f7838f36b
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-08-22 15:56:07 +0100

    adapted for the new internal functions. Code formatting.

--- src/Outsystems.SetupTools/Functions/Get-OSPlatformServerPrivateKey.ps1
+++ src/Outsystems.SetupTools/Functions/Get-OSPlatformServerPrivateKey.ps1
@@ -1,4 +1,4 @@
-Function Get-OSPlatformServerPrivateKey {
+function Get-OSPlatformServerPrivateKey {
     <#
     .SYNOPSIS
     Returns the Outsystems platform server private key.
@@ -6,48 +6,50 @@ Function Get-OSPlatformServerPrivateKey {
     .DESCRIPTION
     This will returns the Outsystems platform server private key. Will throw an exception if the platform is not installed or if the key file doesn't exist.
 
+    .EXAMPLE
+    Get-OSPlatformServerPrivateKey
+
     #>
 
     [CmdletBinding()]
     [OutputType([System.String])]
-    Param()
+    param()
 
-    Begin {
+    begin {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Starting"
-        Try {
-            $InstallDir = GetServerInstallDir
-        }
-        Catch {
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Outsystems platform is not installed"
-            Throw "Outsystems platform is not installed"
-        }
-        LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Server is installed at $InstallDir"
 
-        $Path = "$InstallDir\private.key"
-        If ( -not (Test-Path -Path $Path)) {
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Cant file the file $Path"
-            Throw "Cant file the file $Path"
+        $OSInstallDir = GetServerInstallDir
+        if ($(-not $(GetServerVersion)) -or $(-not $OSInstallDir)){
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Exception $_.Exception -Stream 3 -Message "Outsystems platform is not installed"
+            throw "Outsystems platform is not installed"
+        }
+        LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Server is installed at $OSInstallDir"
+
+        $Path = "$OSInstallDir\private.key"
+        if ( -not (Test-Path -Path $Path)) {
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Cant find the private key at $Path"
+            throw "Cant find the private key at $Path"
         }
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "private.key file found"
     }
 
-    Process {
+    process {
         $Regex = "^--*"
         Get-Content $Path | ForEach-Object {
-            If ( -not ($_ -match $Regex) ) {
+            if ( -not ($_ -match $Regex) ) {
                 $PrivateKey = $_
             }
         }
 
-        If ( -not $PrivateKey ) {
+        if ( -not $PrivateKey ) {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Error processing the file"
-            Throw "Error processing the file"
+            throw "Error processing the file"
         }
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Returning $PrivateKey"
-        Return $PrivateKey
+        return $PrivateKey
     }
 
-    End {
+    end {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 2 -Stream 0 -Message "Ending"
     }
 }
