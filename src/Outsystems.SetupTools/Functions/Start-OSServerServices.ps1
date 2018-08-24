c3211a8dceeaa18dcec45270078f9fbbd3dab7f3
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-08-24 16:31:33 +0100

    code formatting

--- src/Outsystems.SetupTools/Functions/Start-OSServerServices.ps1
+++ src/Outsystems.SetupTools/Functions/Start-OSServerServices.ps1
@@ -1,4 +1,4 @@
-Function Start-OSServerServices {
+function Start-OSServerServices {
     <#
     .SYNOPSIS
     Starts Outsystems services.
@@ -9,22 +9,22 @@ Function Start-OSServerServices {
     #>
 
     [CmdletBinding()]
-    Param()
+    param()
 
-    Begin {
+    begin {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Starting"
-        Try{
+        try{
             CheckRunAsAdmin | Out-Null
         }
-        Catch{
+        catch{
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "The current user is not Administrator or not running this script in an elevated session"
-            Throw "The current user is not Administrator or not running this script in an elevated session"
+            throw "The current user is not Administrator or not running this script in an elevated session"
         }
     }
 
-    Process {
-        ForEach ($OSService in $OSServices) {
-            If ($(Get-Service -Name $OSService -ErrorAction SilentlyContinue)) {
+    process {
+        foreach ($OSService in $OSServices) {
+            if ($(Get-Service -Name $OSService -ErrorAction SilentlyContinue)) {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Starting OS service: $OSService"
                 Get-Service -Name $OSService | Where-Object {$_.StartType -ne "Disabled"} | Start-Service -WarningAction SilentlyContinue
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Service started"
@@ -32,7 +32,7 @@ Function Start-OSServerServices {
         }
     }
 
-    End {
+    end {
         Write-Output "Outsystems services successfully started"
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 2 -Stream 0 -Message "Ending"
     }
