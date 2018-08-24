731fd02ed5eca2f2fb877500ebc9e859a00ee143
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-08-24 17:30:56 +0100

    improved function

--- src/Outsystems.SetupTools/Functions/Start-OSServerServices.ps1
+++ src/Outsystems.SetupTools/Functions/Start-OSServerServices.ps1
@@ -11,28 +11,47 @@ function Start-OSServerServices {
     [CmdletBinding()]
     param()
 
-    begin {
+    begin
+    {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Starting"
-        try{
+        try
+        {
             CheckRunAsAdmin | Out-Null
         }
-        catch{
+        catch
+        {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Exception $_.Exception -Stream 3 -Message "The current user is not Administrator or not running this script in an elevated session"
             throw "The current user is not Administrator or not running this script in an elevated session"
         }
     }
 
-    process {
-        foreach ($OSService in $OSServices) {
-            if ($(Get-Service -Name $OSService -ErrorAction SilentlyContinue)) {
+    process
+    {
+        foreach ($OSService in $OSServices)
+        {
+            if ($(Get-Service -Name $OSService -ErrorAction SilentlyContinue | Where-Object {$_.StartType -ne "Disabled"}))
+            {
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Starting OS service: $OSService"
-                Get-Service -Name $OSService | Where-Object {$_.StartType -ne "Disabled"} | Start-Service -WarningAction SilentlyContinue
+                try
+                {
+                    Get-Service -Name $OSService | Start-Service -WarningAction SilentlyContinue -ErrorAction Stop
+                }
+                catch
+                {
+                    LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Error starting the service $OSService"
+                    throw "Error starting the service $OSService"
+                }
                 LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Service started"
+            }
+            else
+            {
+                LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Service $OSService not found or is disabled. Skipping..."
             }
         }
     }
 
-    end {
+    end
+    {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 2 -Stream 0 -Message "Ending"
     }
 }
