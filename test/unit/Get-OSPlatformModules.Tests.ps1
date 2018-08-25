a66a74dcf08f3409e10fad04e94f627ef996a337
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-08-25 16:19:13 +0100

    added tests

--- test/unit/Get-OSPlatformModules.Tests.ps1
+++ test/unit/Get-OSPlatformModules.Tests.ps1
@@ -31,5 +31,35 @@ InModuleScope -ModuleName OutSystems.SetupTools {
             }
 
         }
+
+        Context 'Can connect and get results using PSCredentials' {
+
+            Mock GetPlatformServicesWS {
+                $obj = [pscustomobject]@{}
+                $obj | Add-Member -MemberType ScriptMethod -Name 'Modules_Get' -Force -Value {
+                    param( [string]$SCUser, [string]$SCPass, [bool]$Dummy1, [bool]$Dummy2 )
+
+                    if ($SCUser -eq 'SuperUser' -and $SCPass -eq 'SuperPass') {
+                        return @{ 'Name' = 'MyModule'; 'Key' = 'c36e9646-0caf-4510-ad35-28a8b97c28b8' }
+                    } else {
+                        throw "Big error"
+                    }
+                }
+                return $obj
+            }
+
+            Mock GetHashedPassword { return $SCPass }
+
+            It 'Should get the app name' {
+                $Credential= New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'SuperUser',('SuperPass' | ConvertTo-SecureString -AsPlainText -Force)
+                $(Get-OSPlatformModules -ServiceCenterHost 255.255.255.255 -Credential $Credential).Name | Should Be "MyModule"
+            }
+
+            It 'Should get the app key' {
+                $Credential= New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'SuperUser',('SuperPass' | ConvertTo-SecureString -AsPlainText -Force)
+                $(Get-OSPlatformModules -ServiceCenterHost 255.255.255.255 -Credential $Credential).Key | Should Be "c36e9646-0caf-4510-ad35-28a8b97c28b8"
+            }
+
+        }
     }
 }
