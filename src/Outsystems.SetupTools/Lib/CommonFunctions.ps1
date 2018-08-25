6ce974cd8e706c53126e68693073444984ab39a7
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-08-25 16:24:57 +0100

    code formatting

--- src/Outsystems.SetupTools/Lib/CommonFunctions.ps1
+++ src/Outsystems.SetupTools/Lib/CommonFunctions.ps1
@@ -1,41 +1,67 @@
-Function LogMessage([string]$Function, [int]$Phase, [int]$Stream, [string]$Message, [object]$Exception){
+function LogMessage([string]$Function, [int]$Phase, [int]$Stream, [string]$Message, [object]$Exception)
+{
 
     # Log types
-    switch ($Phase) {
-        0 { $PhaseMessage += ' [BEGIN  ]' }
-        1 { $PhaseMessage += ' [PROCESS]' }
-        2 { $PhaseMessage += ' [END    ]' }
+    switch ($Phase)
+    {
+        0
+        {
+            $PhaseMessage += ' [BEGIN  ]'
+        }
+
+        1
+        {
+            $PhaseMessage += ' [PROCESS]'
+        }
+
+        2
+        {
+            $PhaseMessage += ' [END    ]'
+        }
     }
 
     $LogLineTemplate = $((get-date).TimeOfDay.ToString()) + " [" + $Function.PadRight(40) + "] $PhaseMessage "
 
-    switch ($Stream) {
-        0 {
+    switch ($Stream)
+    {
+        0
+        {
             Write-Verbose  "$LogLineTemplate $Message"
-            If($script:OSLogFile -and ($script:OSLogFile -ne "")){
+            if ($script:OSLogFile -and ($script:OSLogFile -ne ""))
+            {
                 Add-Content -Path $script:OSLogFile -Value "VERBOSE : $LogLineTemplate $Message"
             }
         }
-        1 {
+
+        1
+        {
             Write-Warning  "$LogLineTemplate $Message"
-            If($script:OSLogFile -and ($script:OSLogFile -ne "")){
+            if ($script:OSLogFile -and ($script:OSLogFile -ne ""))
+            {
                 Add-Content -Path $script:OSLogFile -Value "WARNING : $LogLineTemplate $Message"
             }
         }
-        2 {
+
+        2
+        {
             Write-Debug  "$LogLineTemplate $Message"
-            If($script:OSLogFile -and ($script:OSLogFile -ne "") -and $script:OSLogDebug){
+            if ($script:OSLogFile -and ($script:OSLogFile -ne "") -and $script:OSLogDebug)
+            {
                 Add-Content -Path $script:OSLogFile -Value "DEBUG   : $LogLineTemplate $Message"
             }
         }
-        3 {
+
+        3
+        {
             Write-Verbose "$LogLineTemplate $Message"
 
             # Exception info
-            If ($Exception){
+            if ($Exception)
+            {
                 $E = $Exception
                 Write-Verbose "$LogLineTemplate $($E.Message)"
-                If($script:OSLogFile -and ($script:OSLogFile -ne "")){
+                if ($script:OSLogFile -and ($script:OSLogFile -ne ""))
+                {
                     Add-Content -Path $script:OSLogFile -Value "ERROR   : $LogLineTemplate $Message"
                     Add-Content -Path $script:OSLogFile -Value "InnerException:"
                     Add-Content -Path $script:OSLogFile -Value $($E.Message)
@@ -43,10 +69,12 @@ Function LogMessage([string]$Function, [int]$Phase, [int]$Stream, [string]$Messa
                 }
 
                 # Drill down to show the full exception chain
-                While ($E.InnerException) {
+                while ($E.InnerException)
+                {
                     $E = $E.InnerException
                     Write-Verbose "$LogLineTemplate $($E.Message)"
-                    If($script:OSLogFile -and ($script:OSLogFile -ne "")){
+                    if ($script:OSLogFile -and ($script:OSLogFile -ne ""))
+                    {
                         Add-Content -Path $script:OSLogFile -Value $($E.Message)
                         Add-Content -Path $script:OSLogFile -Value $($E.StackTrace)
                     }
