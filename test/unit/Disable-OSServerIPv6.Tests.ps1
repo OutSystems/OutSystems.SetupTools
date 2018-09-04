3cbd59eee6e86233974a4f46f18dcbe571f5abc7
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-09-04 15:51:33 +0100

    Tests improvementsa

--- test/unit/Disable-OSServerIPv6.Tests.ps1
+++ test/unit/Disable-OSServerIPv6.Tests.ps1
@@ -5,19 +5,18 @@ InModuleScope -ModuleName OutSystems.SetupTools {
     Describe 'Disable-OSServerIPv6 Tests' {
 
         # Global mocks
-        Mock CheckRunAsAdmin {}
+        Mock IsAdmin { return $true }
         Mock Get-NetAdapterBinding {}
         Mock Disable-NetAdapterBinding {}
         Mock New-ItemProperty {}
 
         Context 'When user is not admin' {
 
-            Mock CheckRunAsAdmin { throw "The current user is not Administrator or not running this script in an elevated session" }
+            Mock IsAdmin { return $false }
+            Disable-OSServerIPv6 -ErrorVariable err -ErrorAction SilentlyContinue
 
-            It 'Should not run' {
-                { Disable-OSServerIPv6 } | Should throw "The current user is not Administrator or not running this script in an elevated session"
-            }
-
+            It 'Should output an error' { $err[-1] | Should Be 'The current user is not Administrator or not running this script in an elevated session' }
+            It 'Should not throw' { { Disable-OSServerIPv6 -ErrorAction SilentlyContinue } | Should Not throw }
         }
 
         Context 'When theres an error disabling the IPv6' {
@@ -25,29 +24,19 @@ InModuleScope -ModuleName OutSystems.SetupTools {
             Mock Get-NetAdapterBinding { throw "error" }
             Mock Disable-NetAdapterBinding { throw "error" }
 
-            It 'Should return an exception' {
-                { Disable-OSServerIPv6 } | Should throw "Error disabling IPv6"
-            }
+            Disable-OSServerIPv6 -ErrorVariable err -ErrorAction SilentlyContinue
+
+            It 'Should output an error' { $err[-1] | Should Be 'Error disabling IPv6' }
+            It 'Should not throw' { { Disable-OSServerIPv6 -ErrorAction SilentlyContinue } | Should Not throw }
 
         }
 
         Context 'When everything is fine' {
 
-            It 'Shouldnt throw anything' {
-                { Disable-OSServerIPv6 } | Should Not throw
-            }
+            Disable-OSServerIPv6 -ErrorVariable err -ErrorAction SilentlyContinue
 
-            It 'Should run ok' {
-
-                $assMParams = @{
-                    'CommandName' = 'New-ItemProperty'
-                    'Times'       = 1
-                    'Exactly'     = $true
-                    'Scope'       = 'Context'
-                }
-
-                Assert-MockCalled @assMParams
-            }
+            It 'Should output an error' { $err.Count | Should Be 0 }
+            It 'Should not throw' { { Disable-OSServerIPv6 -ErrorAction SilentlyContinue } | Should Not throw }
 
         }
 
