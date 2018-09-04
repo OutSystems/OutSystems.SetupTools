58fe4a0bec16d194c7e7958acc6038da3b42a40b
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-09-04 15:51:59 +0100

    Removed write-log and adapted error handling

--- src/Outsystems.SetupTools/Functions/Disable-OSServerIPv6.ps1
+++ src/Outsystems.SetupTools/Functions/Disable-OSServerIPv6.ps1
@@ -1,4 +1,5 @@
-function Disable-OSServerIPv6 {
+function Disable-OSServerIPv6
+{
     <#
     .SYNOPSIS
     Disable IPv6.
@@ -15,29 +16,39 @@ function Disable-OSServerIPv6 {
     [CmdletBinding()]
     param()
 
-    begin {
+    begin
+    {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Starting"
-        try {
-            CheckRunAsAdmin | Out-Null
-        } catch {
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "The current user is not Administrator or not running this script in an elevated session"
-            throw "The current user is not Administrator or not running this script in an elevated session"
-        }
     }
 
-    process {
-        try {
+    process
+    {
+        if (-not $(IsAdmin))
+        {
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "The current user is not Administrator or not running this script in an elevated session"
+            WriteNonTerminalError -Message "The current user is not Administrator or not running this script in an elevated session"
+
+            return
+        }
+
+        try
+        {
             # https://support.microsoft.com/en-us/help/929852/guidance-for-configuring-ipv6-in-windows-for-advanced-users
-            Get-NetAdapterBinding -ComponentID 'ms_tcpip6' | Disable-NetAdapterBinding -ComponentID ms_tcpip6
-            New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents" -Value 0xff -PropertyType "DWORD" -Force | Out-Null
-        } catch {
+            Get-NetAdapterBinding -ComponentID 'ms_tcpip6' -ErrorAction SilentlyContinue | Disable-NetAdapterBinding -ComponentID ms_tcpip6 -ErrorAction Stop
+            New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents" -Value 0xff -PropertyType "DWORD" -Force -ErrorAction Stop | Out-Null
+        }
+        catch
+        {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Error disabling IPv6"
-            throw "Error disabling IPv6"
+            WriteNonTerminalError -Message "Error disabling IPv6"
+
+            return
         }
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "IPv6 successfully disabled"
     }
 
-    end {
+    end
+    {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 2 -Stream 0 -Message "Ending"
     }
 }
