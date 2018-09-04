6441d81216ac6dcdd77295f997ac8eab273fe83d
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-09-04 15:40:59 +0100

    Small fix

--- test/unit/Set-OSInstallLog.Tests.ps1
+++ test/unit/Set-OSInstallLog.Tests.ps1
@@ -2,7 +2,7 @@ Get-Module Outsystems.SetupTools | Remove-Module -Force
 Import-Module $PSScriptRoot\..\..\src\Outsystems.SetupTools -Force
 
 InModuleScope -ModuleName OutSystems.SetupTools {
-    Describe 'Set-OSInstallLog.Tests Tests' {
+    Describe 'Set-OSInstallLog Tests' {
         # No tests needed for this CmdLet
     }
 }
