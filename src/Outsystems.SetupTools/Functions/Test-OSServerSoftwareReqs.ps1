0a651d576215169b14d7aaac59286b89bf14616a
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-09-04 14:13:57 +0100

    Added support for OS11

--- src/Outsystems.SetupTools/Functions/Test-OSServerSoftwareReqs.ps1
+++ src/Outsystems.SetupTools/Functions/Test-OSServerSoftwareReqs.ps1
@@ -1,41 +1,86 @@
-Function Test-OSServerSoftwareReqs {
+function Test-OSServerSoftwareReqs
+{
     <#
     .SYNOPSIS
-    Checks if the server has a supported operating system and software installed.
+    Checks if the server has a supported operating system for OutSystems.
 
     .DESCRIPTION
-    This will check if the server has a supported operating system and the right .NET version to run the Outsystems platform.
-    Will throw an exception if the server does not meet the requirements.
+    This will check if the server has a supported operating system to run the Outsystems platform.
+
+    .PARAMETER MajorVersion
+    Specifies the platform major version.
+    The function will install the pre-requisites for the version specified on this parameter. Accepted values: 10.0 or 11.0
+
+    .EXAMPLE
+    Test-OSServerSoftwareReqs -MajorVersion "10.0"
 
     #>
 
     [CmdletBinding()]
-    Param()
+    [OutputType('Outsystems.SetupTools.TestResult')]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('10.0', '11.0')]
+        [string]$MajorVersion
+    )
 
-    Begin {
+    begin
+    {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 0 -Stream 0 -Message "Starting"
+
+        # Initialize the results object
+        $testResult = [pscustomobject]@{
+            PSTypeName = 'Outsystems.SetupTools.TestResult'
+            Result     = $true
+            Message    = 'Operating system was validated for Outsystems'
+        }
     }
 
-    Process {
-        #Check Operating System Version
-        If ([System.Version]$(GetOperatingSystemVersion) -lt [System.Version]$OSReqsMinOSVersion) {
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Operating system not supported. Only Windows Server 2012 and superior is supported"
-            Throw "Operating system not supported. Only Windows Server 2008R2 and superior is supported"
+    process
+    {
+        if ($(GetOperatingSystemProductType) -lt $OSReqsMinOSProductType)
+        {
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Operating system not supported. Only server editions are supported"
+            WriteNonTerminalError -Message "Operating system not supported. Only server editions are supported"
+
+            $testResult.Result = $false
+            $testResult.Message = 'Operating system not supported. Only server editions are supported'
+
+            return $testResult
         }
 
-        #Check Operating System ProductType
-        If ($(GetOperatingSystemProductType) -lt $OSReqsMinOSProductType) {
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Operating system not supported. Only Windows Server 2012 and superior is supported"
-            Throw "Operating system not supported. Only Windows Server 2008R2 and superior is supported"
+        switch ($MajorVersion)
+        {
+            '10.0'
+            {
+                if ([System.Version]$(GetOperatingSystemVersion) -lt [System.Version]$OS10ReqsMinOSVersion)
+                {
+                    LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "This operating system version is not supported for Outsystems $MajorVersion"
+                    WriteNonTerminalError -Message "This operating system version is not supported for Outsystems $MajorVersion"
+
+                    $testResult.Result = $false
+                    $testResult.Message = "This operating system version is not supported for Outsystems $MajorVersion"
+                }
+            }
+            '11.0'
+            {
+                if ([System.Version]$(GetOperatingSystemVersion) -lt [System.Version]$OS11ReqsMinOSVersion)
+                {
+                    LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "This operating system version is not supported for Outsystems $MajorVersion"
+                    WriteNonTerminalError -Message "This operating system version is not supported for Outsystems $MajorVersion"
+
+                    $testResult.Result = $false
+                    $testResult.Message = "This operating system version is not supported for Outsystems $MajorVersion"
+                }
+            }
         }
 
-        LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Operating system is a server product 2012 or higher"
-
-        # TODO: Check PS version
+        LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Operating system validated for Outsystems"
+        $testResult
     }
 
-    End {
-        Write-Output "Your operating systems and software was validated for Outsystems"
+    end
+    {
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 2 -Stream 0 -Message "Ending"
     }
 }
