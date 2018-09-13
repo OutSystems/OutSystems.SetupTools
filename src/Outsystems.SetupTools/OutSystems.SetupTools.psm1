be7fb4e420aa468aa7ae7d96a312441fdb3b38fd
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-09-13 16:48:52 +0100

    Added module arguments

--- src/Outsystems.SetupTools/OutSystems.SetupTools.psm1
+++ src/Outsystems.SetupTools/OutSystems.SetupTools.psm1
@@ -1,8 +1,14 @@
-#Get definition files.
+param(
+    [parameter(Position=0,Mandatory=$false)][boolean]$Telemetry=$true,
+    [parameter(Position=1,Mandatory=$false)][string]$Tier,
+    [parameter(Position=2,Mandatory=$false)][string]$InstKey
+
+)
+# Get definition files.
 $Lib  = @( Get-ChildItem -Path $PSScriptRoot\Lib\*.ps1 -ErrorAction SilentlyContinue )
 $Functions = @( Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue )
 
-#Dot source the files
+# Dot source the files
 Foreach($Import in @($Lib + $Functions))
 {
     Try
@@ -17,6 +23,21 @@ Foreach($Import in @($Lib + $Functions))
 
 # Export only the functions using PowerShell standard verb-noun naming.
 Export-ModuleMember -Function *-*
+
+# Telemetry switch
+$script:OSTelEnabled = $Telemetry
+
+# Add instrumentation key if provided
+if ($Tier)
+{
+    $script:OSTelTier = $Tier
+}
+
+# Add instrumentation key if provided
+if ($InstKey)
+{
+    $script:OSTelAppInsightsKeys += $InstKey
+}
 
 # Send module load event
 SendModuleLoadEvent
