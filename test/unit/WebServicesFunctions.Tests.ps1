ca56249b3b141d4c51d67cf1fa149007faac0e14
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-09-13 16:47:06 +0100

    Changed tests to disable telemetry

--- test/unit/WebServicesFunctions.Tests.ps1
+++ test/unit/WebServicesFunctions.Tests.ps1
@@ -1,6 +1,6 @@
 # This should be changed to import the OS DLLs directly and not importing the module
 Get-Module Outsystems.SetupTools | Remove-Module -Force
-Import-Module $PSScriptRoot\..\..\src\Outsystems.SetupTools -Force
+Import-Module $PSScriptRoot\..\..\src\Outsystems.SetupTools -Force -ArgumentList $false
 
 . $PSScriptRoot\..\..\src\Outsystems.SetupTools\Lib\ServiceCenterWebServices.ps1
 . $PSScriptRoot\..\..\src\Outsystems.SetupTools\Lib\Common.ps1
