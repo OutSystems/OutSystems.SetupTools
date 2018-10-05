24cb104cc85967dba20cf776b149fbd074e1b46f
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-10-05 17:49:39 +0100

    Updated script to the new module version

--- examples/UpdateStack.ps1
+++ examples/UpdateStack.ps1
@@ -1,6 +1,4 @@
 param(
-    [Parameter(Mandatory = $true)][string]$OSServerVersion,
-    [Parameter(Mandatory = $true)][string]$OSServiceStudioVersion,
     [Parameter(Mandatory = $true)][ValidateSet('DC', 'LT', 'FE')][string]$OSRole
 )
 
@@ -13,27 +11,36 @@ $global:ErrorActionPreference = 'Stop'
 
 # -- Import module from Powershell Gallery
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force  | Out-Null
-Install-Module -Name Outsystems.SetupTools -Force | Out-Null
-Import-Module -Name Outsystems.SetupTools -ArgumentList $true, 'UpdateOS' | Out-Null
+Install-Module -Name Outsystems.SetupTools -Force -MinimumVersion 2.2.0.0 | Out-Null
+Import-Module -Name Outsystems.SetupTools -MinimumVersion 2.2.0.0 -ArgumentList $true, 'UpdateOS' | Out-Null
+
+# -- Get platform major version
+$OSServerVersion = Get-OSServerVersion
+if (-not $OSServerVersion) { throw "Platform not installed" }
+$OSServerMajorVersion = "$(([System.Version]$OSServerVersion).Major).$(([System.Version]$OSServerVersion).Minor)"
+
+# -- Check if update is needed
+if ($OSServerVersion -ge $(Get-OSRepoAvailableVersions -Application 'PlatformServer' -MajorVersion $OSServerMajorVersion -Latest))
+{
+    Write-Output 'Platform is up to date'
+    exit 0
+}
 
 # -- Before updating lets refresh outdated modules
 Get-OSPlatformModules -Credential $SCCreds -PassThru -Filter {$_.StatusMessages.Id -eq 6} | Publish-OSPlatformModule -Wait -Verbose | Out-Null
 
 # -- And finally, lets check if the factory is consistent
-if (Get-OSPlatformModules -Credential $SCCreds -PassThru -Filter {$_.StatusMessages.Count -ne 0})
-{
-    throw "Factory is inconsistent. Aborting the update."
-}
+if (Get-OSPlatformModules -Credential $SCCreds -PassThru -Filter {$_.StatusMessages.Count -ne 0}) { throw "Factory is inconsistent. Aborting the update." }
 
 # -- Stop OS Services for the update. Configuration tool will restart them after the update
 Stop-OSServerServices -Verbose
 
 # -- Install PreReqs (this is supposedly not needed since we are updating the same major)
-Install-OSServerPreReqs -MajorVersion "$(([System.Version]$OSServerVersion).Major).$(([System.Version]$OSServerVersion).Minor)" -Verbose | Out-Null
+Install-OSServerPreReqs -MajorVersion $OSServerMajorVersion -Verbose | Out-Null
 
 # -- Download and install OS Server and Dev environment from repo
-Install-OSServer -Version $OSServerVersion -Verbose | Out-Null
-Install-OSServiceStudio -Version $OSServiceStudioVersion -Verbose | Out-Null
+Install-OSServer -Version $(Get-OSRepoAvailableVersions -Application 'PlatformServer' -MajorVersion $OSServerMajorVersion -Latest) -Verbose | Out-Null
+Install-OSServiceStudio -Version $(Get-OSRepoAvailableVersions -Application 'ServiceStudio' -MajorVersion $OSServerMajorVersion -Latest) -Verbose | Out-Null
 
 # -- Run the configuration tool with the existing parameters
 Set-OSServer -Apply -Credential $DBCreds -Verbose | Out-Null
