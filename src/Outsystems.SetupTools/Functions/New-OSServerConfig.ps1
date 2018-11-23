b1b2c86c2d22ddb188cf307cbbd00cdfbc90aa31
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2018-11-23 12:18:11 +0000

    Removed the generation of the templates from the configuration tool because of an issue with the locked file machine.config.

--- src/Outsystems.SetupTools/Functions/New-OSServerConfig.ps1
+++ src/Outsystems.SetupTools/Functions/New-OSServerConfig.ps1
@@ -104,41 +104,17 @@ function New-OSServerConfig
         }
         #endregion
 
-        #region generate templates
-        LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Generating new configuration for database provider $DatabaseProvider"
-
-        try
-        {
-            $result = RunConfigTool -Arguments "/GenerateTemplates"
-        }
-        catch
-        {
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Error lauching the configuration tool"
-            WriteNonTerminalError -Message "Error lauching the configuration tool"
-
-            return $null
-        }
-
-        if ( $result.ExitCode -ne 0 )
-        {
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Error generating the templates. Exit code: $($result.ExitCode)"
-            WriteNonTerminalError -Message "Error generating the templates. Exit code: $($result.ExitCode)"
-
-            return $null
-        }
-        LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Configuration files generated"
-        #endregion
-
         #region copy template
+        $osMajorVersion = "$(([version]$osVersion).Major).$(([version]$osVersion).Minor)"
         switch($DatabaseProvider)
         {
             'SQL'
             {
-                $templateFile = "$osInstallDir\docs\SqlServer_template.hsconf"
+                $templateFile = "$PSScriptRoot\..\Lib\HSConfTemplates\$osMajorVersion\SqlServer_template.hsconf"
             }
             'Oracle'
             {
-                $templateFile = "$osInstallDir\docs\Oracle_template.hsconf"
+                $templateFile = "$PSScriptRoot\..\Lib\HSConfTemplates\$osMajorVersion\Oracle_template.hsconf"
             }
         }
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Copying configuration to the platform server folder"
@@ -154,6 +130,20 @@ function New-OSServerConfig
             return $null
         }
         #endregion
+
+        #region init templates
+        LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Setting initial configuration settings"
+        try
+        {
+            Set-OSServerConfig -SettingSection 'OtherConfigurations' -Setting 'InstallationDir' -Value "$osInstallDir\" -ErrorAction Stop
+            Set-OSServerConfig -SettingSection 'OtherConfigurations' -Setting 'PlatformVersion' -Value $osVersion -ErrorAction Stop
+        }
+        catch
+        {
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Error setting initial configuration settings"
+            WriteNonTerminalError -Message "Error setting initial configuration settings"
+        }
+         #endregion
 
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "New OutSystems configuration successfully created!!"
     }
