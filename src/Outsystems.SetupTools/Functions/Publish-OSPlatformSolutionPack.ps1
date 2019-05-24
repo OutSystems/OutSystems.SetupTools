fb5a62bf0a14b0a2d490cea966262cc9f0642640
DuarteCoelhoOutsystems <49560450+DuarteCoelhoOutsystems@users.noreply.github.com>
2019-05-24 11:05:14 +0100

    cleaned typos

--- src/Outsystems.SetupTools/Functions/Publish-OSPlatformSolutionPack.ps1
+++ src/Outsystems.SetupTools/Functions/Publish-OSPlatformSolutionPack.ps1
@@ -6,7 +6,7 @@ function Publish-OSPlatformSolutionPack
 
     .DESCRIPTION
     This will deploy a solution pack to an OutSystems environment
-    The cmdlet checks for compilation errors and will stop the deployment on any if the Wait switch is specified
+    It will not stop on any error. It proceeds till the end and outputs all errors found during the deployment.
 
     .PARAMETER Solution
     Solution path. This can be an OSP or an OAP file.
@@ -24,7 +24,7 @@ function Publish-OSPlatformSolutionPack
     Publish-OSPlatformSolutionPack -Solution 'c:\solution.osp' -Credential $Credential
 
     .NOTES
-    You can run this cmdlet on any machine with HTTP access to Service Center.
+    This script has to be executed locally on the server in which you wish to publish to. This environment needs to have the osptool present
 
     The cmdlet will return an object with an ExitCode property that will match one of the following values:
     -1 = Error while trying to publish the solution
@@ -66,9 +66,6 @@ function Publish-OSPlatformSolutionPack
         # Initialize the results object
         $publishResult = [pscustomobject]@{
             PSTypeName = 'Outsystems.SetupTools.PublishResult'
-            PublishId  = 0
-            Errors     = 0
-            Warnings   = 0
             Success    = $true
             ExitCode   = 0
             Message    = 'Solution successfully installed'
@@ -88,7 +85,6 @@ function Publish-OSPlatformSolutionPack
 
     process
     {
-
         if ($(-not $osVersion) -or $(-not $osInstallDir))
         {
             LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Outsystems platform is not installed"
@@ -101,42 +97,41 @@ function Publish-OSPlatformSolutionPack
             return $publishResult
         }
 
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Installing Outsystems solution. This can take a while..."
-            try
-            {
-                $result = PublishSolution -Solution $solution -SCUser $ServiceCenterUser -SCPass $ServiceCenterPass
-            }
-            catch
-            {
-                LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Error lauching the solution installion"
-                WriteNonTerminalError -Message "Error lauching the solution installion"
+        LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Installing Outsystems solution. This can take a while..."
+        try
+        {
+            $result = PublishSolution -Solution $solution -SCUser $ServiceCenterUser -SCPass $ServiceCenterPass
+        }
+        catch
+        {
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Exception $_.Exception -Stream 3 -Message "Error lauching the solution installation"
+            WriteNonTerminalError -Message "Error lauching the solution installation"
 
-                $publishResult.Success = $false
-                $publishResult.ExitCode = -1
-                $publishResult.Message = 'Error lauching the solution installion'
+            $publishResult.Success = $false
+            $publishResult.ExitCode = -1
+            $publishResult.Message = 'Error lauching the solution installation'
 
-                return $publishResult
-            }
+            return $publishResult
+        }
 
-            $outputLog = $($result.Output) -Split ("`r`n")
-            foreach ($logline in $outputLog)
-            {
-                LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "OSPTOOL: $logline"
-            }
-            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "OSPTool exit code: $($result.ExitCode)"
+        $outputLog = $($result.Output) -Split ("`r`n")
+        foreach ($logline in $outputLog)
+        {
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "OSPTOOL: $logline"
+        }
+        LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "OSPTool exit code: $($result.ExitCode)"
 
-            if ( $result.ExitCode -ne 0 )
-            {
-                LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Error installing the solution. Return code: $($result.ExitCode)"
-                WriteNonTerminalError -Message "Error installing the solution. Return code: $($result.ExitCode)"
+        if ( $result.ExitCode -ne 0 )
+        {
+            LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 3 -Message "Error installing the solution. Return code: $($result.ExitCode)"
+            WriteNonTerminalError -Message "Error installing the solution. Return code: $($result.ExitCode)"
 
-                $publishResult.Success = $false
-                $publishResult.ExitCode = $result.ExitCode
-                $publishResult.Message = 'Error installing the solution'
+            $publishResult.Success = $false
+            $publishResult.ExitCode = $result.ExitCode
+            $publishResult.Message = 'Error installing the solution'
 
-                return $publishResult
-            }
-
+            return $publishResult
+        }
 
         LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 0 -Message "Solution successfully installed!!"
         return $publishResult
--- test/unit/Publish-OSPlatformSolutionPack.Tests.ps1
+++ test/unit/Publish-OSPlatformSolutionPack.Tests.ps1
@@ -9,7 +9,6 @@ InModuleScope -ModuleName OutSystems.SetupTools {
         Mock GetServerVersion { return '10.0.0.1' }
         Mock GetServerInstallDir { return 'C:\Program Files\OutSystems\Platform Server' }
 
-
         $assRunPublishSolution = @{ 'CommandName' = 'PublishSolution'; 'Times' = 1; 'Exactly' = $true; 'Scope' = 'Context'; 'ParameterFilter' = { $SCUser -eq "admin" -and $SCPass -eq "admin" } }
         $assNotRunPublishSolution = @{ 'CommandName' = 'PublishSolution'; 'Times' = 0; 'Exactly' = $true; 'Scope' = 'Context' }
 
@@ -18,7 +17,7 @@ InModuleScope -ModuleName OutSystems.SetupTools {
             Mock GetServerVersion { return $null }
             Mock GetServerInstallDir { return $null }
 
-            $result = Publish-OSPlatformSolutionPack -Solution "hi" -ErrorVariable err -ErrorAction SilentlyContinue
+            $result = Publish-OSPlatformSolutionPack -Solution "solutionDummy" -ErrorVariable err -ErrorAction SilentlyContinue
 
             It 'Should not run the installation' { Assert-MockCalled @assNotRunPublishSolution}
             It 'Should return the right result' {
@@ -27,12 +26,12 @@ InModuleScope -ModuleName OutSystems.SetupTools {
                 $result.Message | Should Be 'Outsystems platform is not installed'
             }
             It 'Should output an error' { $err[-1] | Should Be 'Outsystems platform is not installed' }
-            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "hi" -ErrorAction SilentlyContinue } | Should Not throw }
+            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "solutionDummy" -ErrorAction SilentlyContinue } | Should Not throw }
         }
 
         Context 'When the solution installation succeeds' {
 
-            $result = Publish-OSPlatformSolutionPack -Solution "hi"  -ErrorVariable err -ErrorAction SilentlyContinue
+            $result = Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorVariable err -ErrorAction SilentlyContinue
 
             It 'Should run the installation' { Assert-MockCalled @assRunPublishSolution }
             It 'Should return the right result' {
@@ -41,32 +40,30 @@ InModuleScope -ModuleName OutSystems.SetupTools {
                 $result.Message | Should Be 'Solution successfully installed'
             }
             It 'Should not output an error' { $err.Count | Should Be 0 }
-            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "hi"  -ErrorAction SilentlyContinue } | Should Not throw }
+            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorAction SilentlyContinue } | Should Not throw }
         }
 
         Context 'When theres an error launching the publish solution' {
 
-            Mock GetSysComponentsCompiledVersion { return $null }
             Mock PublishSolution { throw "Error" }
 
-            $result = Publish-OSPlatformSolutionPack -Solution "hi"  -ErrorVariable err -ErrorAction SilentlyContinue
+            $result = Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorVariable err -ErrorAction SilentlyContinue
 
             It 'Should run the installation' { Assert-MockCalled @assRunPublishSolution }
             It 'Should return the right result' {
                 $result.Success | Should Be $false
                 $result.ExitCode | Should Be -1
-                $result.Message | Should Be 'Error lauching the solution installion'
+                $result.Message | Should Be 'Error lauching the solution installation'
             }
-            It 'Should output an error' { $err[-1] | Should Be 'Error lauching the solution installion' }
-            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "hi"  -ErrorAction SilentlyContinue } | Should Not throw }
+            It 'Should output an error' { $err[-1] | Should Be 'Error lauching the solution installation' }
+            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorAction SilentlyContinue } | Should Not throw }
         }
 
         Context 'When theres an error installing a solution' {
 
-            Mock GetSysComponentsCompiledVersion { return $null }
             Mock PublishSolution { return @{ 'Output' = 'NOT good'; 'ExitCode' = 1} }
 
-            $result = Publish-OSPlatformSolutionPack -Solution "hi"  -ErrorVariable err -ErrorAction SilentlyContinue
+            $result = Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorVariable err -ErrorAction SilentlyContinue
 
             It 'Should run the installation' { Assert-MockCalled @assRunPublishSolution }
             It 'Should return the right result' {
@@ -75,31 +72,40 @@ InModuleScope -ModuleName OutSystems.SetupTools {
                 $result.Message | Should Be 'Error installing the solution'
             }
             It 'Should output an error' { $err[-1] | Should Be 'Error installing the solution. Return code: 1' }
-            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "hi"  -ErrorAction SilentlyContinue } | Should Not throw }
+            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorAction SilentlyContinue } | Should Not throw }
         }
 
         Context 'When ServiceCenterUser is specified' {
 
-            Mock GetSysComponentsCompiledVersion { return $null }
-
-            $result = Publish-OSPlatformSolutionPack -Solution "hi"  -ServiceCenterUser 'Whatever' -ErrorVariable err -ErrorAction SilentlyContinue
-
+            $result = Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ServiceCenterUser 'Whatever' -ErrorVariable err -ErrorAction SilentlyContinue
             $assRunPublishSolution = @{ 'CommandName' = 'PublishSolution'; 'Times' = 1; 'Exactly' = $true; 'Scope' = 'Context'; 'ParameterFilter' = { $SCUser -eq "Whatever" -and $SCPass -eq "admin" } }
 
             It 'Should run the installation with other parameters' { Assert-MockCalled @assRunPublishSolution }
+            It 'Should run the installation' { Assert-MockCalled @assRunPublishSolution }
+            It 'Should return the right result' {
+                $result.Success | Should Be $true
+                $result.ExitCode | Should Be 0
+                $result.Message | Should Be 'Solution successfully installed'
+            }
+            It 'Should not output an error' { $err.Count | Should Be 0 }
+            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorAction SilentlyContinue } | Should Not throw }
         }
 
         Context 'When credentials are specified' {
 
-            Mock GetSysComponentsCompiledVersion { return $null }
-
             $cred = New-Object System.Management.Automation.PSCredential ("Whatever", $(ConvertTo-SecureString "admin" -AsPlainText -Force))
 
-            $result = Publish-OSPlatformSolutionPack -Solution "hi" -Credential $cred -ErrorVariable err -ErrorAction SilentlyContinue
-
+            $result = Publish-OSPlatformSolutionPack -Solution "solutionDummy" -Credential $cred -ErrorVariable err -ErrorAction SilentlyContinue
             $assRunPublishSolution = @{ 'CommandName' = 'PublishSolution'; 'Times' = 1; 'Exactly' = $true; 'Scope' = 'Context'; 'ParameterFilter' = { $SCUser -eq "Whatever" -and $SCPass -eq "admin" } }
 
+            It 'Should return the right result' {
+                $result.Success | Should Be $true
+                $result.ExitCode | Should Be 0
+                $result.Message | Should Be 'Solution successfully installed'
+            }
             It 'Should run the installation with other parameters' { Assert-MockCalled @assRunPublishSolution }
+            It 'Should not output an error' { $err.Count | Should Be 0 }
+            It 'Should not throw' { { Publish-OSPlatformSolutionPack -Solution "solutionDummy"  -ErrorAction SilentlyContinue } | Should Not throw }
         }
     }
 }
