a4e6259117f983782077285e51436e8dac190016
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2019-08-07 22:20:19 +0100

    Another small fix

--- src/Outsystems.SetupTools/Lib/Constants.ps1
+++ src/Outsystems.SetupTools/Lib/Constants.ps1
@@ -15,8 +15,8 @@ $OSWindowsFeaturesBase = @(
     "Web-Default-Doc", "Web-Dir-Browsing", "Web-Http-Errors", "Web-Static-Content",
     "Web-Http-Logging", "Web-Request-Monitor",
     "Web-Stat-Compression", "Web-Dyn-Compression",
-    "Web-Filtering", "Web-Windows-Auth", `
-        "Web-Net-Ext45", "Web-Asp-Net45", "Web-ISAPI-Ext", "Web-ISAPI-Filter",
+    "Web-Filtering", "Web-Windows-Auth",
+    "Web-Net-Ext45", "Web-Asp-Net45", "Web-ISAPI-Ext", "Web-ISAPI-Filter",
     "Web-Metabase",
     "WAS-Config-APIs", "WAS-Process-Model"
 )
