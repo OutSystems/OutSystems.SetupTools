94803050b74ddcb2979d0c78e0fa184b0310f849
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2020-03-17 14:53:47 +0000

    Fixed date/time format in logs

--- src/Outsystems.SetupTools/Lib/Common.ps1
+++ src/Outsystems.SetupTools/Lib/Common.ps1
@@ -18,7 +18,7 @@ function LogMessage([string]$Function, [int]$Phase, [int]$Stream, [string]$Messa
         }
     }
 
-    $logLineTemplate = $((get-date).TimeOfDay.ToString()) + " [" + $Function.PadRight(40) + "] $phaseMessage "
+    $logLineTemplate = $(Get-Date -Format "yyyy-MM-dd HH:mm:ss.ffff") + " [" + $Function.PadRight(40) + "] $phaseMessage "
 
     switch ($Stream)
     {
