d5922afbeca238f2b44557027359e80c32f152dc
Pedro Nunes <37883272+pintonunes@users.noreply.github.com>
2020-06-05 14:55:24 +0100

    Small fixes

--- src/Outsystems.SetupTools/Lib/PlatformSetup.ps1
+++ src/Outsystems.SetupTools/Lib/PlatformSetup.ps1
@@ -591,7 +591,7 @@ Function RunConfigTool([string]$Arguments, [scriptblock]$OnLogEvent)
 
     LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 2 -Message "Running the config tool..."
 
-    $Result = ExecuteCommand -CommandPath "$InstallDir\ConfigurationTool.com" -WorkingDirectory $InstallDir -CommandArguments "$Arguments" -OnLogEvent $onLogEvent
+    $Result = ExecuteCommand -CommandPath "$InstallDir\ConfigurationTool.com" -WorkingDirectory $InstallDir -CommandArguments "$Arguments" -OnLogEvent $OnLogEvent
     LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 2 -Message "Exit code: $($Result.ExitCode)"
 
     Return $Result
@@ -605,7 +605,7 @@ function RunSCInstaller([string]$Arguments, [scriptblock] $OnLogEvent)
     LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 2 -Message "Running SCInstaller..."
 
     #SCInstaller needs to run inside a CMD or will not return an exit code
-    $result = ExecuteCommand -CommandPath "$env:comspec" -WorkingDirectory $installDir -CommandArguments "/c SCInstaller.exe $Arguments && exit /b %ERRORLEVEL%" -OnLogEvent $onLogEvent -ErrorAction Stop
+    $result = ExecuteCommand -CommandPath "$env:comspec" -WorkingDirectory $installDir -CommandArguments "/c SCInstaller.exe $Arguments && exit /b %ERRORLEVEL%" -OnLogEvent $OnLogEvent -ErrorAction Stop
 
     LogMessage -Function $($MyInvocation.Mycommand) -Phase 1 -Stream 2 -Message "Exit code: $($result.ExitCode)"
 
