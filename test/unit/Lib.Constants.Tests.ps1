a50c49d976abc81e7416e311e3ea12c2d2dbc1cb
Nuno Silva <nuno.ribeiro.silva@outsystems.com>
2021-05-11 11:13:05 +0100

    R11PIT-160 - Fix failing tests

--- test/unit/Lib.Constants.Tests.ps1
+++ test/unit/Lib.Constants.Tests.ps1
@@ -51,7 +51,7 @@ Describe 'Lib Constants Tests' {
     Context 'Check .NETCore constants' {
 
         $SavePath = "$env:TEMP\dotnetcore.exe"
-        $FileHash = 'AC74CADB690D3A5A175CEDD0EF02A11ABE41A292F9C9055B28522E3EB7B02726'
+        $FileHash = '187179215D0C9DE82F6C6F005E08AC517E34E9689959964053B0F60FEDFD8302'
 
         It 'Should be downloadable and have the right file hash' {
             (New-Object System.Net.WebClient).DownloadFile($OSRepoURLDotNETCore, $SavePath)
