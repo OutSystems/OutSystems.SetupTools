588840126e11df8de681cc30ae585a51260d8bf9
OS-diogomachado <diogo.machado@outsystems.com>
2023-05-02 14:55:19 +0100

    R11OT-531 Fixed bug where versions above 11 were not properly handled

--- src/Outsystems.SetupTools/Functions/Install-OSPlatformServiceCenter.ps1
+++ src/Outsystems.SetupTools/Functions/Install-OSPlatformServiceCenter.ps1
@@ -90,13 +90,13 @@ function Install-OSPlatformServiceCenter
                 }
                 { ($_  -as [int]) -ge 11}
                 {
-                    if (($(([version]$osVersion).Minor) -lt 18) -or ($(([version]$osVersion).Minor) -eq 18 -and $(([version]$osVersion).Build) -lt 1))
+                    if ($(([version]$osVersion).Major -eq 11) -and (($(([version]$osVersion).Minor) -lt 18) -or ($(([version]$osVersion).Minor) -eq 18 -and $(([version]$osVersion).Build) -lt 1)))
                     {
                         $scInstallerArguments = '-file ServiceCenter.oml -extension OMLProcessor.xif IntegrationStudio.xif PlatformLogs.xif'
                     }
                     else
                     {
-                        $scInstallerArguments = '-file ServiceCenter.oml -extension OMLProcessor.xif IntegrationStudio.xif CentralizedPlatformLogs.xif'
+                        $scInstallerArguments = '-file ServiceCenter.oml -extension OMLProcessor.xif IntegrationStudio.xif PlatformLogs.xif CentralizedPlatformLogs.xif'
                     }
                 }
                 default
