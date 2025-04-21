313fd62af157641902eea49b5166ca94406786fc
Sérgio Miranda <143180482+sergiomiranda-outsystems@users.noreply.github.com>
2025-04-21 16:40:35 +0100

    Add tests for Get-OSRepoAvailableVersions

---
+++ test/unit/Get-OSRepoAvailableVersions.Tests.ps1
@@ -0,0 +1,48 @@
+Get-Module Outsystems.SetupTools | Remove-Module -Force
+Import-Module $PSScriptRoot\..\..\src\Outsystems.SetupTools -Force -ArgumentList $false, '', '', $false
+
+InModuleScope -ModuleName OutSystems.SetupTools {
+    Describe 'Get-OSRepoAvailableVersions Tests' {
+
+        Context 'When getting Platform Server versions' {
+
+            $result = Get-OSRepoAvailableVersions -Application 'PlatformServer' -MajorVersion '11' -ErrorVariable err -ErrorAction SilentlyContinue
+
+            It 'Should not have errors' { $err.Count | Should Be 0 }
+            It 'Should return at least one version' { $result.Count | Should BeGreaterThan 0 }
+        }
+
+        Context 'When getting Development Environment versions' {
+
+            $result = Get-OSRepoAvailableVersions -Application 'DevelopmentEnvironment' -MajorVersion '11' -ErrorVariable err -ErrorAction SilentlyContinue
+
+            It 'Should not have errors' { $err.Count | Should Be 0 }
+            It 'Should return at least one version' { $result.Count | Should BeGreaterThan 0 }
+        }
+
+        Context 'When getting Lifetime versions' {
+
+            $result = Get-OSRepoAvailableVersions -Application 'Lifetime' -MajorVersion '11' -ErrorVariable err -ErrorAction SilentlyContinue
+
+            It 'Should not have errors' { $err.Count | Should Be 0 }
+            It 'Should return at least one version' { $result.Count | Should BeGreaterThan 0 }
+        }
+
+        Context 'When getting Integration Studio versions' {
+
+            $result = Get-OSRepoAvailableVersions -Application 'IntegrationStudio' -MajorVersion '11' -ErrorVariable err -ErrorAction SilentlyContinue
+
+            It 'Should not have errors' { $err.Count | Should Be 0 }
+            It 'Should return at least one version' { $result.Count | Should BeGreaterThan 0 }
+        }
+
+        Context 'When getting Service Studio versions' {
+
+            $result = Get-OSRepoAvailableVersions -Application 'ServiceStudio' -MajorVersion '11' -ErrorVariable err -ErrorAction SilentlyContinue
+
+            It 'Should not have errors' { $err.Count | Should Be 0 }
+            It 'Should return at least one version' { $result.Count | Should BeGreaterThan 0 }
+        }
+
+    }
+}
