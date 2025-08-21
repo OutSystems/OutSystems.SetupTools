a77a2383322a083964c7184b8854ba3bf4f413c5
Alex Peregrina <47484650+ajps28@users.noreply.github.com>
2025-08-21 11:06:41 +0200

    R11BRT-1934 Stop disabling FIPS (#154)
    * Disable FIPS only for versions below 11.38.0
    
    * Update Install-OSServerPreReqs.Tests.ps1
    
    * Update Install-OSServerPreReqs.Tests.ps1

--- test/unit/Install-OSServerPreReqs.Tests.ps1
+++ test/unit/Install-OSServerPreReqs.Tests.ps1
@@ -228,7 +228,7 @@ InModuleScope -ModuleName OutSystems.SetupTools {
             It 'Should run the .NET 8.0 Hosting Bundle installation' { Assert-MockCalled @assRunInstallDotNetHostingBundle8 }
             It 'Should configure the WMI service' { Assert-MockCalled @assRunConfigureServiceWMI }
             It 'Should configure the Windows search service' { Assert-MockCalled @assRunConfigureServiceWindowsSearch }
-            It 'Should disable the FIPS' { Assert-MockCalled @assRunDisableFIPS }
+            It 'Should not disable the FIPS' { Assert-MockCalled @assNotRunDisableFIPS }
             It 'Should configure the windows event log' { Assert-MockCalled @assRunConfigureWindowsEventLog }
             It 'Should not configure the MSMQ' { Assert-MockCalled @assNotRunConfigureMSMQDomainServer }
             It 'Should return the right result' {
@@ -259,7 +259,7 @@ InModuleScope -ModuleName OutSystems.SetupTools {
             It 'Should not run the .NET 8.0 Hosting Bundle installation' { Assert-MockCalled @assNotRunInstallDotNetHostingBundle8 }
             It 'Should configure the WMI service' { Assert-MockCalled @assRunConfigureServiceWMI }
             It 'Should configure the Windows search service' { Assert-MockCalled @assRunConfigureServiceWindowsSearch }
-            It 'Should disable the FIPS' { Assert-MockCalled @assRunDisableFIPS }
+            It 'Should not disable the FIPS' { Assert-MockCalled @assNotRunDisableFIPS }
             It 'Should configure the windows event log' { Assert-MockCalled @assRunConfigureWindowsEventLog }
             It 'Should not configure the MSMQ' { Assert-MockCalled @assNotRunConfigureMSMQDomainServer }
             It 'Should return the right result' {
@@ -1761,6 +1761,34 @@ InModuleScope -ModuleName OutSystems.SetupTools {
                 $result.Message | Should Be 'OutSystems platform server pre-requisites successfully installed'
             }
             It 'Should not throw' { { Install-OSServerPreReqs -MajorVersion '11' -ErrorVariable err -ErrorAction SilentlyContinue } | Should Not throw }
+        }
+
+        Context 'When trying to install prerequisites for a OS 11 version in Minor version 38 and Patch version 0 (11.38.0), it should not disable FIPS' {
+
+            $result = Install-OSServerPreReqs -MajorVersion '11' -MinorVersion '38' -PatchVersion '0' -ErrorVariable err -ErrorAction SilentlyContinue
+
+            It 'Should run the .NET installation' { Assert-MockCalled @assRunInstallDotNet }
+            It 'Should not run the BuildTools installation' { Assert-MockCalled @assNotRunInstallBuildTools }
+            It 'Should install the windows features installation' { Assert-MockCalled @assRunInstallWindowsFeatures }
+            It 'Should not run the .NET core 2.1 installation' { Assert-MockCalled @assNotRunInstallDotNetCore21 }
+            It 'Should not run the .NET core installation' { Assert-MockCalled @assNotRunInstallDotNetCore }
+            It 'Should not run the .NET 6.0 Hosting Bundle installation' { Assert-MockCalled @assNotRunInstallDotNetHostingBundle }
+            It 'Should run the .NET 8.0 Hosting Bundle installation' { Assert-MockCalled @assRunInstallDotNetHostingBundle8 }
+            It 'Should not run the .NET Core Uninstall Tool installation' { Assert-MockCalled @assNotRunInstallDotNetCoreUninstallTool }
+            It 'Should configure the WMI service' { Assert-MockCalled @assRunConfigureServiceWMI }
+            It 'Should configure the Windows search service' { Assert-MockCalled @assRunConfigureServiceWindowsSearch }
+            It 'Should not disable the FIPS' { Assert-MockCalled @assNotRunDisableFIPS }
+            It 'Should configure the windows event log' { Assert-MockCalled @assRunConfigureWindowsEventLog }
+            It 'Should not configure the MSMQ' { Assert-MockCalled @assNotRunConfigureMSMQDomainServer }
+
+            It 'Should return the right result' {
+                $result.Success | Should Be $true
+                $result.RebootNeeded | Should Be $false
+                $result.ExitCode | Should Be 0
+                $result.Message | Should Be 'OutSystems platform server pre-requisites successfully installed'
+            }
+
+            It 'Should not throw' { { Install-OSServerPreReqs -MajorVersion '11' -MinorVersion '38' -PatchVersion '0' -ErrorVariable err -ErrorAction SilentlyContinue } | Should Not throw }
         }
     }
 }
